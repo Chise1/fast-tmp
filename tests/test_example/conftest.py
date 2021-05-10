817b3778a42022c5228046370d4b017805e58635
Chise1 <chise123@live.com>
2021-05-10 17:06:48 +0800

    增加测试项目

---
+++ tests/test_example/conftest.py
@@ -0,0 +1,16 @@
+import os
+
+import pytest
+
+from tortoise.contrib.test import finalizer, initializer
+
+
+@pytest.fixture(scope="session", autouse=True)
+def initialize_tests(request):
+    db_url = os.environ.get("TORTOISE_TEST_DB", "sqlite://:memory:")
+    initializer(
+        ["fast_tmp.models","test_example.models"],
+        db_url=db_url,
+        app_label="fast_tmp",
+    )
+    request.addfinalizer(finalizer)
---
+++ tests/test_example/test_example/app.py
@@ -0,0 +1,11 @@
+import os
+
+os.environ.setdefault("FASTAPI_SETTINGS_MODULE", 'test_example.settings')
+from test_example.factory import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run(app, debug=True, port=8000, lifespan="on")
