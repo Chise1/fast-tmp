075597768b117570d7ea028cd1f20710926c5cec
Chise1 <chise123@live.com>
2021-05-22 16:41:57 +0800

    把tortoise-orm里面的pydantic_model_creator提取出来了，并增加了深度概念，修复了重复使用导致模型submodel错误的bug.

---
+++ fast_tmp/utils/pydantic/__init__.py
@@ -0,0 +1,3 @@
+from tortoise.contrib.pydantic.base import PydanticListModel, PydanticModel  # noqa
+from tortoise.contrib.pydantic.creator import pydantic_model_creator  # noqa
+from tortoise.contrib.pydantic.creator import pydantic_queryset_creator
