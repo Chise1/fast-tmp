533e9aa87af332ce113d0982e9ac9fcd087c5a5f
Chise1 <chise123@live.com>
2021-06-03 13:24:43 +0800

    v0.1版本的页面生成器

--- fast_tmp/admin/creator.py
+++ fast_tmp/admin/creator.py
@@ -7,11 +7,11 @@ from fast_tmp.admin.schema.crud import CRUD
 from fast_tmp.admin.schema.enums import ButtonLevelEnum
 from fast_tmp.admin.schema.forms import Form
 from fast_tmp.admin.schema.frame import Dialog
-from fast_tmp.admin.schema.page import App, AppPage, AppPageGroup
+from fast_tmp.admin.schema.page import AppPage, AppPageGroup, Page
+from fast_tmp.admin.utils import get_columns_from_model, get_controls_from_model
 
 
 class AbstractApp:
-    app: App
     page_groups: Dict[str, AppPageGroup]
     _instance = None
 
@@ -26,26 +26,26 @@ class AbstractApp:
         logo: Optional[str],
         prefix: str = "",
     ):
-        self.app = App(
-            brandName=name,
-            pages=[],
-            logo=logo,
-        )
         self.page_groups = {"default": AppPageGroup(label="default", children=[])}
-        self.pages = {}
+        self.abstract_cruds = {}
         self.prefix = prefix
+        self.page_list = []
 
-    def get_json(self):
-        self.app.pages = [i for i in self.page_groups]
-        return self.app.json()
+    def dict(self):
+        res = []
+        for group_name, group in self.page_groups.items():
+            res.append(group.dict(exclude_none=True))
+        return res
 
-    def add_page(self, page: AppPage, group_name: str = "default"):
-        self.page_groups[group_name].children.append(page)
+    def add_page(self, page: "AbstractCRUD", group_name: str = "default"):
+        p = page.get_AppPage()
+        self.page_groups[group_name].children.append(p)
+        self.abstract_cruds[page.name] = page
 
     def add_page_group(self, group: AppPageGroup):
         self.page_groups[group.label] = group
 
-    def get_page(self, model_name) -> AppPage:
+    def get_Page(self, model_name) -> AppPage:
         for group_name, page_group in self.page_groups.items():
             for page in page_group.children:
                 if page.label == model_name:
@@ -53,16 +53,15 @@ class AbstractApp:
         else:
             raise ModuleNotFoundError(f"Can't found {model_name}")  # fixme:完善报错信息为http类型
 
-
-from fast_tmp.admin.utils import get_columns_from_model, get_controls_from_model
+    def get_AbstractCRUD(self, name):
+        return self.abstract_cruds[name]
 
 
 class AbstractCRUD:
-    body = []
-
     def __init__(
         self,
         model: Type[Model],
+        prefix: str = "/admin",
         create_include: Tuple[str, ...] = (),
         create_exclude: Tuple[str, ...] = (),
         up_include: Tuple[str, ...] = (),
@@ -74,6 +73,12 @@ class AbstractCRUD:
         """
         抽象crud-json生成
         """
+        self.body = []
+        self.model = model
+        self.prefix = prefix
+        self.name = model.__name__
+        self.list_include = list_exclude
+        self.up_include = up_include
         buttons = []
         if "Create" in methods:
             self.body.append(
@@ -83,10 +88,12 @@ class AbstractCRUD:
                         title="新增",
                         body=Form(
                             name=f"新增{model.__name__}",
+                            title=f"新增{model.__name__}",
                             controls=get_controls_from_model(
                                 model, include=create_include, exclude=create_exclude
                             ),
-                            api=f"post:{model.__name__}/create",
+                            primaryField=model._meta.pk_attr,
+                            api=f"post:{prefix}/{model.__name__}/create",
                         ),
                     ),
                 )
@@ -102,8 +109,8 @@ class AbstractCRUD:
                             controls=get_controls_from_model(
                                 model, include=up_include, exclude=up_exclude
                             ),
-                            api="put:" + model.__name__ + "/up/${id}",
-                            initApi="get:" + model.__name__ + "/up?id=${id}",
+                            api="put:" + prefix + "/" + model.__name__ + "/update/${id}",
+                            initApi="get:" + prefix + "/" + model.__name__ + "/update/${id}",
                         ),
                     ),
                 )
@@ -111,20 +118,32 @@ class AbstractCRUD:
         if "Delete" in methods:
             buttons.append(
                 AjaxAction(
-                    api="delete:" + model.__name__ + "/delete/${id}",
+                    api="delete:" + prefix + "/" + model.__name__ + "/delete/${id}",
                     label="删除",
                     level=ButtonLevelEnum.danger,
                 )
             )
         if "List" in methods:
+            columns = get_columns_from_model(model, include=list_include, exclude=list_exclude)
+            columns.extend(buttons)
             self.body.append(
                 CRUD(
-                    api="get:" + model.__name__ + "/list",
-                    columns=get_columns_from_model(
-                        model, include=list_include, exclude=list_exclude
-                    ).extend(buttons),
+                    api="get:" + prefix + "/" + model.__name__ + "/list",
+                    columns=columns,
                 )
             )
         if "DeleteMany" in methods:
             # fixme:尚未实现
             pass
+
+    def get_AppPage(self):
+        return AppPage(
+            label=self.model.__name__,
+            icon="",
+            url=self.prefix + "/" + self.model.__name__,
+            schema=Page(title=self.model.__name__, body=self.body)
+            # schemaApi="get:" + self.prefix + "/" + self.model.__name__ + "/schema",
+        )
+
+    def get_Page(self):
+        return Page(title=self.model.__name__, body=self.body)
