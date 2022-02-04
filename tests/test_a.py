eb5c32345d28f7ca6777092be447877412cb8ab7
yuxf <yuxf@unitechs.com>
2022-02-04 23:46:19 +0800

    add test login

--- tests/test_a.py
+++ tests/test_a.py
@@ -1,3 +1,17 @@
+from fastapi.testclient import TestClient
+from fastapi import FastAPI
 
-def test_init():
-    pass
+from fast_tmp.admin.server import admin
+
+app = FastAPI()
+app.mount("/admin", admin, name="admin", )
+
+
+def test_admin_user():
+    client = TestClient(app)
+    login_res = client.get("/admin/login")
+    assert login_res.status_code == 200
+    assert login_res.text.count("<h2 class=\"card-title text-center mb-4\">Login to your account</h2>") == 1
+    login_post_res = client.post("/admin/login", data={"username": "root", "password": "root"})
+    assert login_post_res.status_code == 307
+    assert login_post_res.headers.get("location") == 'http://testserver/admin/'
