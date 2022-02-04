7f06a7700aa64e292e84fead4baa275313d335a9
yuxf <yuxf@unitechs.com>
2022-02-04 21:57:58 +0800

    fix example

--- example/example/app.py
+++ example/example/app.py
@@ -6,9 +6,9 @@ from fast_tmp.models import Base
 
 os.environ.setdefault("FASTAPI_SETTINGS_MODULE", 'settings')
 from fast_tmp.admin.server import admin
-from example.factory import create_app
+from .factory import create_app
 from fast_tmp.site import register_model_site
-from example.admin import UserInfoAdmin
+from .admin import UserInfoAdmin
 
 register_model_site({"Example":[UserInfoAdmin]})
 app: FastAPI = create_app()
