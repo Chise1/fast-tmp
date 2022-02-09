562f94bca7664203e2f8240ae89e949ed93cde56
yuxf <yuxf@unitechs.com>
2022-02-09 11:24:34 +0800

    增加登出功能和使用本地文件的方法.

--- fast_tmp/admin/server.py
+++ fast_tmp/admin/server.py
@@ -3,7 +3,6 @@ from datetime import timedelta
 from typing import Optional
 
 from fastapi import Depends, FastAPI, Form, Request
-from fastapi.security import OAuth2PasswordBearer
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from starlette.responses import RedirectResponse
@@ -22,7 +21,6 @@ from .constant import crud_root_rooter, model_router
 from .endpoint import router
 from .responses import BaseRes
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl=settings.LOGIN_URL)
 base_path = os.path.dirname(__file__)
 templates = Jinja2Templates(directory=base_path + "/templates")
 register_tags(templates)
@@ -108,6 +106,19 @@ def login_html(request: Request):
         "password_err": False,
     }
     return templates.TemplateResponse("login.html", context)
+
+
+@admin.get("/logout", name="logout")
+def logout(request: Request):
+    context = {
+        "request": request,
+        "errinfo": "",
+        "username_err": False,
+        "password_err": False,
+    }
+    res = templates.TemplateResponse("login.html", context)
+    res.delete_cookie("access_token")
+    return res
 
 
 @admin.get("/site")
--- fast_tmp/jinja_extension/tags.py
+++ fast_tmp/jinja_extension/tags.py
@@ -1,6 +1,8 @@
 import jinja2
 from pydantic import typing
 
+from fast_tmp.conf import settings
+
 
 def register_tags(templates):
     env = templates.env
@@ -12,3 +14,13 @@ def register_tags(templates):
         return request.url_for(path, **path_params)
 
     env.globals["static"] = static
+
+    # 是否使用本地静态文件
+
+    @jinja2.pass_context
+    def local_file(
+        context: dict,
+    ) -> bool:
+        return settings.LOCAL_STATIC
+
+    env.globals["local_file"] = local_file
