e86dfaba953f031903bc49ee1341f1f7d6392e11
yuxf <yuxf@unitechs.com>
2022-02-10 18:23:54 +0800

    移动返回类到根目录

--- fast_tmp/responses.py
+++ fast_tmp/responses.py
@@ -1,5 +1,5 @@
 from typing import Any
-
+from fastapi import HTTPException
 from pydantic import BaseModel
 
 
@@ -11,3 +11,7 @@ class BaseRes(BaseModel):
 
 key_error = BaseRes(status=400, msg="主键错误")
 not_found_instance = BaseRes(status=404, msg="找不到对象")
+not_found_model = HTTPException(
+    status_code=200,
+    detail=BaseRes(msg="找不到对象").dict()
+)
