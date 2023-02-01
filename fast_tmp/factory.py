82d0a2b49d44a4bc9266991517afcaec76272d40
yuxf <yuxf@unitechs.com>
2023-02-01 16:52:50 +0800

    简化create_app

--- fast_tmp/factory.py
+++ fast_tmp/factory.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
 
 from fast_tmp.admin.server import admin
 from fast_tmp.conf import settings
@@ -11,18 +10,6 @@ def create_app() -> FastAPI:
     :return: FastAPI
     """
     app = FastAPI(debug=settings.DEBUG)
-    # 注册app的位置
-    if settings.BACKEND_CORS_ORIGINS:
-        app.add_middleware(
-            CORSMiddleware,
-            allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
-            allow_credentials=True,
-            allow_methods=["*"],
-            allow_headers=["*"],
-        )
-    # 在cookie存储session
-    # app.add_middleware(SessionMiddleware, secret_key=settings.CAS_SESSION_SECRET)
-
     app.mount("/admin", admin, name="admin")
 
     return app
